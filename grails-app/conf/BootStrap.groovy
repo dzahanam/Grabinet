21d0b9b310a952d65e2ef3b6b4439721fa1554a5
Michał Olczak <michal.olczak@bzwbk.pl>
2011-06-09 16:42:53 +0200

    DB data from CSV import

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,15 @@
+import java.text.ParseException
+
 import gabinet.Address
 import gabinet.Client
+import java.text.SimpleDateFormat
 
 import org.codehaus.groovy.grails.commons.ApplicationHolder
 import org.gabinet.SecRole
 import org.gabinet.SecUser
 import org.gabinet.SecUserSecRole
+
+
 
 class BootStrap {
 	def springSecurityService
@@ -13,40 +18,45 @@ class BootStrap {
 		// Check whether the test data already exists.
 		if (!Client.count()) {
 			def address = new Address(city: "Poznan", street : "ulica", postalCode : "61-100").save(failOnError: true)
-			new Client(
-				firstName: "Stephen King", 
-				surName: "The Shining", 
-				email: "example@org.pl", 
-				allergy: "", 
-				birthDate: new Date(), 
-				cellPhone: "", 
-				comment: "", 
-				family: "", 
-				homePhone: "", 
-				recommendation: "", 
-				workPhone: "", 
-				address: address).save(failOnError: true)
-			new Client(firstName: "Józef", surName: "Dobronocki", email: "j.dobro@gazeta.pl", recommendation: "recommendation1").save(failOnError: true)
-//			def filePath = "resources/klienci2.csv"
-//			def text = ApplicationHolder.application.parentContext.getResource("classpath:$filePath").inputStream.text
-//			text.eachLine {
-//				println it
-//				  def ClientFields = it.split(",")
-//				println ClientFields
-//				  new Client(firstName: ClientFields[1],
-//			             email: "",
-//						 surName: ClientFields[2],
-//						 homePhone: ClientFields[3],
-//						 workPhone: ClientFields[4],
-//						 cellPhone: ClientFields[5],
-//						 allergy: ClientFields[6],
-//						 birthDate: ClientFields[7],
-//						 family: ClientFields[8],
-//						 recommendation: ClientFields[9],
-////			          other: ClientFields[10],	
-//						 address: address
-//			          ).save()
-//			}
+//			new Client(
+//				firstName: "Stephen King", 
+//				surName: "The Shining", 
+//				email: "example@org.pl", 
+//				allergy: "", 
+//				birthDate: new Date(), 
+//				cellPhone: "", 
+//				comment: "", 
+//				family: "", 
+//				homePhone: "", 
+//				recommendation: "", 
+//				workPhone: "", 
+//				address: address).save(failOnError: true)
+			def filePath = "resources/klienci.csv"
+			def text = ApplicationHolder.application.parentContext.getResource("classpath:$filePath").inputStream.text
+			text.eachCsvLine {
+				tokens ->
+				println "tokens: "+tokens + " size: "+tokens.size()
+				SimpleDateFormat format = new SimpleDateFormat("yy-MM-dd")
+				Date date = new Date()
+				if (tokens[7]!="")
+					try {
+						date = format.parse(tokens[7])					
+					} catch (ParseException e) {
+					}
+				new Client(firstName: tokens[1]!="" ? tokens[1] : "DU",
+			             email: "",
+						 surName: tokens[2]!="" ? tokens[2] : "DU",
+						 homePhone: tokens[3],
+						 workPhone: tokens[4],
+						 cellPhone: tokens[5],
+						 allergy: tokens[6],
+						 birthDate: date,
+						 family: tokens[8],
+						 recommendation: tokens[9],
+						 comment: tokens[10],
+						 address: address
+			          ).save(failOnError: true)
+			}
 
 		}
 		
---
+++ src/groovy/util/date/DateFormatter.groovy
@@ -0,0 +1,5 @@
+package util.date
+
+class DateFormatter {
+
+}
